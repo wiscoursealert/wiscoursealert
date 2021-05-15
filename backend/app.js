e0a5780579549fa42c3b33beeca7a62c01bca7c1
Robin Deeboonchai <comtalyst@outlook.com>
2021-05-16 02:05:29 +0700

    Add loader for app.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,10 @@
+//// Load modules
+require('./loaders')
+
 //// Load express
 const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors');
-
-      
-//// Load config
-require('dotenv/config')
-
-
-//// Load database
-const mongoose = require('mongoose');
-require('./models/Users');
-require('./models/Courses');
-
-mongoose.connect(
-  process.env.MONGODB_URI, 
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, 
-  () => console.log('Connected to DB.')
-);
-
-
-//// Load other services
-require('./services/updater');
-
 
 //// Initialize app
 const app = express();
--- backend/loaders/index.js
+++ backend/loaders/index.js
@@ -1,21 +1,26 @@
-//////////////////////////
-//// Define loaders
-//////////////////////////
+//// Load config
+require('dotenv/config')
 
-// init database
-dbLoader = () => {
-  // trigger database's initialization
-  require('../models/ModelInterface.js')
-  return true
-}
+//// Load database
+const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MONGODB_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, 
+  () => console.log('Connected to DB.')
+);
 
+//// Load models
+require('../models/Users.js');
+require('../models/Courses.js');
 
-//////////////////////////
-//// Run loaders
-//////////////////////////
+//// Load services
+require('../services/linkGenerator.js')
+require('../services/mailer.js')
+require('../services/register.js')
+require('../services/responder.js')
+require('../services/subscriber.js')
+require('../services/updater.js')
 
-if(dbLoader()){
-  console.log("Loader: Database initialized")
-}
-
-
+//// Load subscribers
+require('../subscribers/mailer.js')
+require('../subscribers/responder.js')
