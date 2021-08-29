f663115cbcf999540a974ba9243e77a492ccf78b
Robin Deeboonchai <comtalyst@outlook.com>
2021-08-29 16:17:37 -0500

    Create the landing logo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Register from './pages/Register';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Register />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/pages/register.js
@@ -0,0 +1,15 @@
+import React, {useRef} from 'react';
+
+import { Box, Heading, Flex } from '@chakra-ui/react'
+
+function Register() {
+  return (
+    <Box bg='white' color='#595959'> 
+      <Flex>
+        <Heading color='#D8524E'>wisc</Heading><Heading>oursealert</Heading>
+      </Flex>
+    </Box>
+  );
+}
+
+export default Register;
