3aa271f472010344619b12df4394331b9e7e1311
Robin Deeboonchai <comtalyst@outlook.com>
2021-12-11 17:16:28 -0600

    Validate UUID in link generator

--- backend/services/linkGenerator.js
+++ backend/services/linkGenerator.js
@@ -1,4 +1,12 @@
+validateUUID = (user_id) => {
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  return regexExp.test(user_id);
+}
+
 generateLink = (user_id) => {
+  if(!validateUUID(user_id)){
+    throw new Error("User ID is not a valid UUID.");
+  }
   let editUrl = process.env.URL_MAIN + "/?token=" + user_id;
   return editUrl;
 };
