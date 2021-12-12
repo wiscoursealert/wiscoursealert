95f5cc8c2e1f8a76d5ad43d9a2cbc2119f774192
Robin Deeboonchai <comtalyst@outlook.com>
2021-12-12 14:35:22 -0600

    Add proper unit test for LinkGenerator

---
+++ backend/tests/services/LinkGenerator.test.js
@@ -0,0 +1,16 @@
+const LinkGenerator = require('../../services/LinkGenerator');
+
+describe("Testing Service LinkGenerator", () => {
+  test("Generates link correctly if UUID is valid", () => {
+    const mockUserId = "2b15adb2-f644-4cf1-afb4-a670cac7461d";
+    expect(LinkGenerator(mockUserId)).toBe(process.env.URL_MAIN + "/?token=2b15adb2-f644-4cf1-afb4-a670cac7461d");
+  });
+  test("Throws error if UUID is invalid: too long", () => {
+    const mockUserId = "2b15adb2-f644-4cf1-afb4-a670cac7461dx";
+    expect(() => LinkGenerator(mockUserId)).toThrowError();
+  });
+  test("Throws error if UUID is invalid: invalid format", () => {
+    const mockUserId = "2b15adb29f64424cf1vafb4za670cac7461d";
+    expect(() => LinkGenerator(mockUserId)).toThrowError();
+  });
+})
