ab0c279c95c77c1dff5108bc77218d2df185554f
Robin Deeboonchai <comtalyst@outlook.com>
2021-12-26 18:01:38 -0600

    Clean Subscriber Responder test

--- backend/tests/subscribers/Responder.test.js
+++ backend/tests/subscribers/Responder.test.js
@@ -1,105 +1,18 @@
-const Mongoose = require('../mocks/mongoose')
+const Responder = require('../../subscribers/Responder');
 const MockResponderService = require('../mocks/services/Responder');
 // mock redis is broken, but real redis works fine
-const Responder = require('../../subscribers/responder')
-const CoursesModel = require('../../models/Courses')
-const UsersModel = require('../../models/Users')
 
 wait = async (ms) => {
   await new Promise(resolve => setTimeout(resolve, ms));
 }
 
-insertMockDbData1 = async () => {
-  await CoursesModel.addCourse({
-    course_id: "CS101",
-    course_name: "Outro to Quantum Super Machine-Learned Cryptocurrency",
-    subject_id: "COMPSCI007",
-    sections: [{
-      section_id: "CS101_S01",
-      subscribers: [
-        { email: "guy1-1@mail.com", last_sent: Date.now() - 10*(60*1000)},
-        { email: "guy1-2@mail.com", last_sent: Date.now() - 20*(60*1000)}
-      ],
-      status: "OPEN",
-      prev_status: "unknown"
-    },
-    {
-      section_id: "CS101_S02",
-      subscribers: [
-        { email: "guy2-1@mail.com", last_sent: Date.now() - 10*(60*1000)},
-        { email: "guy2-2@mail.com", last_sent: Date.now() - 20*(60*1000)}
-      ],
-      status: "WAITLISTED",
-      prev_status: "unknown"
-    },
-    {
-      section_id: "CS101_S03",
-      subscribers: [
-        { email: "guy3-1@mail.com", last_sent: Date.now() - 10*(60*1000)},
-        { email: "guy3-2@mail.com", last_sent: Date.now() - 20*(60*1000)}
-      ],
-      status: "WAITLISTED",
-      prev_status: "unknown"
-    },
-    {
-      section_id: "CS101_S04",
-      subscribers: [
-        { email: "guy4-1@mail.com", last_sent: Date.now() - 10*(60*1000)},
-        { email: "guy4-2@mail.com", last_sent: Date.now() - 20*(60*1000)}
-      ],
-      status: "CLOSED",
-      prev_status: "unknown"
-    }]
-  })
-  //const result = await CoursesModel.getAll()
-  //console.log(result)
-
-  await UsersModel.addUser({
-    user_id: "U1-1",
-    email: "guy1-1@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U1-2",
-    email: "guy1-2@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U2-1",
-    email: "guy2-1@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U2-2",
-    email: "guy2-2@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U3-1",
-    email: "guy3-1@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U3-2",
-    email: "guy3-2@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U4-1",
-    email: "guy4-1@mail.com"
-  })
-  await UsersModel.addUser({
-    user_id: "U4-2",
-    email: "guy4-2@mail.com"
-  })
-}
-
 beforeAll(async () => {
   jest.setTimeout(30000);
-  await Mongoose.connect()
 });
-afterEach(async () => await Mongoose.clearDatabase());
-afterAll(async () => await Mongoose.closeDatabase());
 
 describe("Testing Subscriber Responder", () => {
   test("Successfully enqueue and forward to the service", async () => {
-    // define test data
-    await insertMockDbData1()
-    mockApiResult = [
+    const mockApiResult = [
       {
         "id": "CS101_S01",
         "courseId": "CS101",
@@ -153,16 +66,6 @@ describe("Testing Subscriber Responder", () => {
         ]
       }
     ]
-    const expected = [{
-      email: "guy2-2@mail.com",
-      section_id: "CS101_S02"
-    },
-    {
-      email: "guy4-2@mail.com",
-      section_id: "CS101_S04"
-    }]
-
-    // enqueue and wait for processing
     await Responder(mockApiResult, MockResponderService.service);
     await wait(5000);
 
