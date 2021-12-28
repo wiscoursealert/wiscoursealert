47d9d5b8de895fecbc2287cda460daa08f782241
Robin Deeboonchai <comtalyst@outlook.com>
2021-12-27 22:16:57 -0600

    Add a unit test for Responder

--- backend/tests/services/Responder.test.js
+++ backend/tests/services/Responder.test.js
@@ -5,10 +5,6 @@ const Responder = require('../../services/Responder')
 const CoursesModel = require('../../models/Courses')
 const UsersModel = require('../../models/Users')
 
-beforeAll(async () => await Mongoose.connect());
-afterEach(async () => await Mongoose.clearDatabase());
-afterAll(async () => await Mongoose.closeDatabase());
-
 insertMockDbData1 = async () => {
   await CoursesModel.addCourse({
     course_id: "CS101",
@@ -51,8 +47,6 @@ insertMockDbData1 = async () => {
       prev_status: "unknown"
     }]
   })
-  //const result = await CoursesModel.getAll()
-  //console.log(result)
 
   await UsersModel.addUser({
     user_id: "U1-1",
@@ -87,85 +81,114 @@ insertMockDbData1 = async () => {
     email: "guy4-2@mail.com"
   })
 }
+wait = async (ms) => {
+  await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+beforeAll(async () => await Mongoose.connect());
+beforeEach(async () => await insertMockDbData1());
+afterEach(async () => await Mongoose.clearDatabase());
+afterAll(async () => await Mongoose.closeDatabase());
 
 describe("Testing Service Responder", () => {
-  test("Responder responds correctly", async () => {
-    await insertMockDbData1()
-    mockApiResult = [
-      {
-        "id": "CS101_S01",
-        "courseId": "CS101",
-        "packageEnrollmentStatus": {
-          "status": "OPEN"
-        },
-        "sections": [
-          {
-            "type": "LEC",
-            "sectionNumber": "001"
-          }
-        ]
+  const mockApiResult = [
+    {
+      "id": "CS101_S01",
+      "courseId": "CS101",
+      "packageEnrollmentStatus": {
+        "status": "OPEN"
       },
-      {
-        "id": "CS101_S02",
-        "courseId": "CS101",
-        "packageEnrollmentStatus": {
-          "status": "OPEN"
-        },
-        "sections": [
-          {
-            "type": "LEC",
-            "sectionNumber": "002"
-          }
-        ]
-      },
-      {
-        "id": "CS101_S03",
-        "courseId": "CS101",
-        "packageEnrollmentStatus": {
-          "status": "WAITLISTED"
-        },
-        "sections": [
-          {
-            "type": "LEC",
-            "sectionNumber": "003"
-          }
-        ]
-      },
-      {
-        "id": "CS101_S04",
-        "courseId": "CS101",
-        "packageEnrollmentStatus": {
-          "status": "WAITLISTED"
-        },
-        "sections": [
-          {
-            "type": "LEC",
-            "sectionNumber": "004"
-          }
-        ]
-      }
-    ]
-  
-    const expected = [{
-      email: "guy2-2@mail.com",
-      section_id: "CS101_S02"
+      "sections": [
+        {
+          "type": "LEC",
+          "sectionNumber": "001"
+        }
+      ]
     },
     {
-      email: "guy4-2@mail.com",
-      section_id: "CS101_S04"
-    }]
+      "id": "CS101_S02",
+      "courseId": "CS101",
+      "packageEnrollmentStatus": {
+        "status": "OPEN"
+      },
+      "sections": [
+        {
+          "type": "LEC",
+          "sectionNumber": "002"
+        }
+      ]
+    },
+    {
+      "id": "CS101_S03",
+      "courseId": "CS101",
+      "packageEnrollmentStatus": {
+        "status": "WAITLISTED"
+      },
+      "sections": [
+        {
+          "type": "LEC",
+          "sectionNumber": "003"
+        }
+      ]
+    },
+    {
+      "id": "CS101_S04",
+      "courseId": "CS101",
+      "packageEnrollmentStatus": {
+        "status": "WAITLISTED"
+      },
+      "sections": [
+        {
+          "type": "LEC",
+          "sectionNumber": "004"
+        }
+      ]
+    }
+  ]
+  const expected = [{
+    email: "guy2-2@mail.com",
+    section_id: "CS101_S02"
+  },
+  {
+    email: "guy4-2@mail.com",
+    section_id: "CS101_S04"
+  }]
 
+  test("Responder responds correctly", async () => {
     await Responder(mockApiResult, Mailer);
-    let res = Mailer.notifyList;
-    res = res.map((mailData) => {
+    let mails = Mailer.notifyList;
+    mails = mails.map((mailData) => {
       let x = {
         email: mailData.user_email,
         section_id: mailData.section_id
       };
       return x;
     })
-    res.sort((x, y) => (x.email > y.email)? 1:-1)
+    mails.sort((x, y) => (x.email > y.email)? 1:-1)
   
-    expect(res).toMatchObject(expected)
+    expect(mails).toMatchObject(expected)
+  })
+
+  test("Responder changes last_sent for notified users correctly", async () => {
+    const approxSentTime = Date.now();
+    await Responder(mockApiResult, Mailer);
+    await wait(1000);
+    const expectedSet = new Set(expected.map(({email, section_id}) => email + ";" + section_id));
+
+    const courses = await CoursesModel.getAll();
+    //console.log("approxSentTime  >>> " + approxSentTime)
+    await Promise.all(courses.map(async ({sections}) => {
+      sections.map(({section_id, subscribers}) => {
+        subscribers.map(({email, last_sent}) => {
+          //console.log(email + ";" + section_id + "  >>> " + last_sent.getTime())
+          if(expectedSet.has(email + ";" + section_id)){
+            expect(last_sent.getTime()).toBeGreaterThanOrEqual(approxSentTime);
+          }
+          else{
+            expect(last_sent.getTime()).toBeLessThan(approxSentTime);
+          }
+        })
+      })
+    }))
   })
 })
