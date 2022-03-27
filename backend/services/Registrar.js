c55702ff8b2317e68ae9b52b751dd289a39df702
comtalyst <comtalyst@outlook.com>
2022-03-27 15:09:24 -0500

    Mails portal to the user after registration

--- backend/services/Registrar.js
+++ backend/services/Registrar.js
@@ -2,6 +2,8 @@ const UsersModel = require("../models/Users");
 const Subscriber = require("./Subscriber");
 const { v4: uuid_v4 } = require("uuid");
 
+const Mailer = require("../subscribers/Mailer")
+
 const Registrar = {};
 
 Registrar.getUser = async (user_id) => {
@@ -16,12 +18,16 @@ Registrar.getUser = async (user_id) => {
 Registrar.addUser = async (email) => {
   const users = await UsersModel.findEmail(email);
   if (users.length === 0) {
+    let uuid = uuid_v4();
     const newUser = await UsersModel.addUser({
-      user_id: uuid_v4(),
+      user_id: uuid,
       email: email,
     });
+    Mailer.portal({user_email: email, user_id: uuid});
     return newUser;
   } else {
+    
+    Mailer.portal({user_email: email, user_id: users[0].user_id});
     return users[0];
   }
 };
