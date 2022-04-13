7a0c17a8f3c0e6bff4a39bf9d6e7554e00621f40
comtalyst <comtalyst@outlook.com>
2022-04-12 23:19:16 -0500

    Prevent error in each section and each course

--- backend/services/responder.js
+++ backend/services/responder.js
@@ -12,7 +12,13 @@ const run = async (results, mailer=null) => {
 };
 
 const processCourses = async (results, mailer) => {
-  results.map(course => processCourse(course, mailer));
+  results.map(course => {
+    try{
+      return processCourse(course, mailer);
+    } catch (e){
+      console.error(e);
+    }
+  });
 }
 
 const processCourse = async (results, mailer) => {
@@ -66,65 +72,69 @@ const processCourse = async (results, mailer) => {
   const course_name = course_data.course_name;
 
   for (let i = 0; i < course_data.sections.length; i++) {
-    let section_data = course_data.sections[i];
+    try{
+      let section_data = course_data.sections[i];
 
-    // get section id
-    const sec_id = section_data.section_id;
+      // get section id
+      const sec_id = section_data.section_id;
 
-    // locate new data of this section
-    const section = secs_dict[sec_id];
-    if (typeof section === "undefined") {
-      console.log("Section data not received from updater, skipping");
-      continue;
-    }
+      // locate new data of this section
+      const section = secs_dict[sec_id];
+      if (typeof section === "undefined") {
+        console.log("Section data not received from updater, skipping");
+        continue;
+      }
 
-    // update status
-    section_data.prev_status = section_data.status;
-    section_data.status = section.status;
+      // update status
+      section_data.prev_status = section_data.status;
+      section_data.status = section.status;
 
-    // should notify?
-    if (
-      (section_data.prev_status == "CLOSED" &&
-        section_data.status == "WAITLISTED") ||
-      (section_data.prev_status == "CLOSED" && section_data.status == "OPEN") ||
-      (section_data.prev_status == "WAITLISTED" &&
-        section_data.status == "OPEN")
-    ) {
-      // find each person
-      for (let j = 0; j < section_data.subscribers.length; j++) {
-        let subscriber = section_data.subscribers[j];
+      // should notify?
+      if (
+        (section_data.prev_status == "CLOSED" &&
+          section_data.status == "WAITLISTED") ||
+        (section_data.prev_status == "CLOSED" && section_data.status == "OPEN") ||
+        (section_data.prev_status == "WAITLISTED" &&
+          section_data.status == "OPEN")
+      ) {
+        // find each person
+        for (let j = 0; j < section_data.subscribers.length; j++) {
+          let subscriber = section_data.subscribers[j];
 
-        // get user details
-        const email = subscriber.email;
-        const last_sent = subscriber.last_sent;
-        const users = await usersRepository.findEmail(email);
+          // get user details
+          const email = subscriber.email;
+          const last_sent = subscriber.last_sent;
+          const users = await usersRepository.findEmail(email);
 
-        if (users.length == 0) continue;
+          if (users.length == 0) continue;
 
-        const user = users[0]; // user
-        const delay = user.delay; // minutes
-        const user_id = user.user_id;
+          const user = users[0]; // user
+          const delay = user.delay; // minutes
+          const user_id = user.user_id;
 
-        // check if last_sent and delay is proper
-        const current_time = Date.now();
-        if ((current_time - last_sent) / (1000 * 60) >= delay) {
-          subscriber.last_sent = current_time; // set last_sent to now
+          // check if last_sent and delay is proper
+          const current_time = Date.now();
+          if ((current_time - last_sent) / (1000 * 60) >= delay) {
+            subscriber.last_sent = current_time; // set last_sent to now
 
-          // mail this person
-          const mailParams = {
-            user_id: user_id,
-            user_email: email,
-            course_name: course_name,
-            lecture_name: section.lec_num,
-            discussion_name: section.dis_num,
-            section_id: sec_id,
-            lab_name: section.lab_num,
-            prev_status: section_data.prev_status,
-            new_status: section_data.status,
-          };
-          mailer.notify(mailParams); // no need to await for anything
+            // mail this person
+            const mailParams = {
+              user_id: user_id,
+              user_email: email,
+              course_name: course_name,
+              lecture_name: section.lec_num,
+              discussion_name: section.dis_num,
+              section_id: sec_id,
+              lab_name: section.lab_num,
+              prev_status: section_data.prev_status,
+              new_status: section_data.status,
+            };
+            mailer.notify(mailParams); // no need to await for anything
+          }
         }
       }
+    } catch (e){
+      console.error(e);
     }
   }
   // update database
